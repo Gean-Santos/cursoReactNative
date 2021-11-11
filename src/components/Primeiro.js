e00d779b9a4c32549224f587e2f758af7912e308
Gean-Santos <geansantos_15@hotmail.com>
2021-11-11 19:37:47 -0300

    Exercises of style

---
+++ src/components/Primeiro.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Text } from 'react-native';
+import estilo from './estilo';
+
+import Estilo from './estilo';
+
+export default () => <Text style={estilo.txtgrande}>Primeiro!</Text>
