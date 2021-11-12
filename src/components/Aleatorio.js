295f5865744d2de2a355932f0e8ca9959bd89629
Gean-Santos <geansantos_15@hotmail.com>
2021-11-12 00:18:16 -0300

    Random challenge

--- src/components/Aleatorio.js
+++ src/components/Aleatorio.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Text } from 'react-native';
 import Estilo from './estilo';
 
-export default Aleatorio = props => {
-    const { min, max } = props;
+export default Aleatorio = ({ min, max }) => {
     const numAleatorio = Math.floor(Math.random() * (max - min + 1) + min)
 
     return(
